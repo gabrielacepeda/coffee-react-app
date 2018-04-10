7caf1d31c985139f233b745b2ba1c9d7a2eb4513
Gabriela Cepeda <gabcep14@gmail.com>
2018-04-10 00:55:59 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://demo:demo@ds125896.mlab.com:25896/coffee' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
