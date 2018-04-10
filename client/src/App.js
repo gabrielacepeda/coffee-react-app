0b8bea8cd363d764be42d8f87b25a7b2be61a9fa
gabrielacepeda <gabcep14@gmail.com>
2018-04-10 00:26:57 -0400

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Messages from './Messages/messages.js';
 
 class App extends Component {
+  state = {users: []}
+
+ componentDidMount() {
+    fetch('/api/messages',{
+     headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+     .then(res => res.json())
+     .then(users => this.setState({ users }));
+  }
 
   render() {
     return (
-      <div>
-        <Messages color='blue'></Messages>
+      <div className="App">
+        <h1>orders</h1>
+        {this.state.users.map(user =>
+          <div key={user.name}>{user.order}</div>
+        )}
       </div>
     );
   }
